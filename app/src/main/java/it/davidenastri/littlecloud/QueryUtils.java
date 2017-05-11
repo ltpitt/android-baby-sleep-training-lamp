93ff5e32609084232b3ca7f21e2d2abb958ff708
Davide Nastri <d.nastri@gmail.com>
2017-05-11 16:26:57 +0200

    feat: Implementd Async Http call

---
+++ app/src/main/java/it/davidenastri/littlecloud/QueryUtils.java
@@ -0,0 +1,11 @@
+package it.davidenastri.littlecloud;
+
+/**
+ * Created by DNastri on 5/11/2017.
+ */
+
+public class QueryUtils {
+
+
+
+}
--- app/src/main/java/it/davidenastri/littlecloud/TabLight.java
+++ app/src/main/java/it/davidenastri/littlecloud/TabLight.java
@@ -3,13 +3,19 @@ package it.davidenastri.littlecloud;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
 
 
 /**
@@ -38,11 +44,53 @@ public class TabLight extends Fragment{
             public void onClick(View v) {
 
                 int color = colorPicker.getColor();
-                String rgbString = "R: " + Color.red(color) + " B: " + Color.blue(color) + " G: " + Color.green(color);
+                String rgbString = Color.red(color) + "," + Color.green(color) + "," +
+                        Color.blue(color);
                 Log.e("RGB color code is: ", rgbString);
 
+                /*
                 Snackbar.make(v, "RGB color code is: " + rgbString, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                */
+
+
+    // Http call
+
+                String PARTICLE_DEVICE_ID = "310031000447353138383138";
+                String PARTICLE_TOKEN_ID = "1d89ba47f0c5c9ee72b7ebb12ac171bf5c1f9234";
+                String PARTICLE_API_URL = "https://api.particle.io/v1/devices/";
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                params.put("access_token", PARTICLE_TOKEN_ID);
+                params.put("args", rgbString);
+                client.post(PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor", params, new TextHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, String res) {
+                                // called when response HTTP status is "200 OK"
+                                Log.e("Done","Cool! :D");
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                                Log.e("Dead","Not cool :(");
+                            }
+                        }
+                );
+
+                // Http call
+
+
+
+
+
+
+
+
+
+
+
+
 
             }
 
