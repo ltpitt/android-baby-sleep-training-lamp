d91bae72df5b83de60cb193591fc24635fb0f997
Davide Nastri <d.nastri@gmail.com>
2017-05-12 14:41:02 +0200

    feat: Added circular ProgressBar to FAB, prevented multiple requests

--- app/src/main/java/it/davidenastri/littlecloud/QueryUtils.java
+++ app/src/main/java/it/davidenastri/littlecloud/QueryUtils.java
@@ -4,8 +4,68 @@ package it.davidenastri.littlecloud;
  * Created by DNastri on 5/11/2017.
  */
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
+import static com.loopj.android.http.AsyncHttpClient.LOG_TAG;
+
+/**
+ * Created by DNastri on 5/11/2017.
+ */
+
 public class QueryUtils {
 
 
+    // Http call
+    final String PARTICLE_DEVICE_ID = "310031000447353138383138";
+    final String PARTICLE_TOKEN_ID = "1d89ba47f0c5c9ee72b7ebb12ac171bf5c1f9234";
+    final String PARTICLE_API_URL = "https://api.particle.io/v1/devices/";
+    AsyncHttpClient client = new AsyncHttpClient();
 
+    QueryUtils() {
+
+    }
+
+    public void executeQuery(String rgbString, final String colorSet, final View v, final View rootView  ) {
+
+
+        final FloatingActionButton floatingActionButton;
+        floatingActionButton = (FloatingActionButton) rootView.findViewById(R.id.fab);
+        final RequestParams params = new RequestParams();
+        params.put("access_token", PARTICLE_TOKEN_ID);
+        params.put("args", rgbString);
+        final ProgressBar spinner;
+        spinner = (ProgressBar)rootView.findViewById(R.id.progressBarLight);
+        client.post(PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor", params, new TextHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String res) {
+                // called when response HTTP status is "200 OK"
+                Log.i(LOG_TAG, "Lamp color was successfully changed. Cool! :D");
+                Toast.makeText(rootView.getContext(), colorSet, Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, "Lamp color changed successfully, yey! :D", Snackbar.LENGTH_LONG).show();
+                spinner.setVisibility(View.GONE);
+                floatingActionButton.show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Toast.makeText(rootView.getContext(), PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor" + params, Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, "A terrible error prevented lamp color change :(", Snackbar.LENGTH_LONG).show();
+                Log.e(LOG_TAG, PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor" + params);
+                spinner.setVisibility(View.GONE);
+                floatingActionButton.show();
+            }
+        });
+    }
 }
--- app/src/main/java/it/davidenastri/littlecloud/TabLight.java
+++ app/src/main/java/it/davidenastri/littlecloud/TabLight.java
@@ -1,14 +1,17 @@
 package it.davidenastri.littlecloud;
 
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestParams;
@@ -16,6 +19,7 @@ import com.loopj.android.http.TextHttpResponseHandler;
 
 import cz.msebera.android.httpclient.Header;
 
+import static it.davidenastri.littlecloud.R.id.fab;
 
 
 /**
@@ -24,6 +28,10 @@ import cz.msebera.android.httpclient.Header;
 
 public class TabLight extends Fragment{
 
+    /**
+     * Log tag for EditorActivity
+     */
+    private static final String LOG_TAG = TabLight.class.getSimpleName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,66 +39,64 @@ public class TabLight extends Fragment{
         final View rootView = inflater.inflate(R.layout.tab_light, container, false);
 
         final ColorPicker colorPicker;
-        final Button button;
         final FloatingActionButton floatingActionButton;
 
         colorPicker = (ColorPicker) rootView.findViewById(R.id.colorPicker);
 
-        floatingActionButton = (FloatingActionButton) rootView.findViewById(R.id.fab);
+        floatingActionButton = (FloatingActionButton) rootView.findViewById(fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
 
-                int color = colorPicker.getColor();
-                String rgbString = Color.red(color) + "," + Color.green(color) + "," +
-                        Color.blue(color);
-                Log.e("RGB color code is: ", rgbString);
+                final ProgressBar spinner;
+                spinner = (ProgressBar)rootView.findViewById(R.id.progressBarLight);
 
-                /*
-                Snackbar.make(v, "RGB color code is: " + rgbString, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                */
+                // Prevent multiple requests by the user if another request is happening
+                if (spinner.getVisibility() == View.INVISIBLE) {
+                    spinner.getIndeterminateDrawable().setColorFilter(Color.BLUE, PorterDuff.Mode.MULTIPLY);
+                    spinner.setVisibility(View.VISIBLE);
 
+                    int color = colorPicker.getColor();
+                    String rgbString = Color.red(color) + "," + Color.green(color) + "," +
+                            Color.blue(color);
+                    final String colorSet = "Red: " + Color.red(color) + "Green: " + Color.green(color) +
+                            "Blue: " + Color.blue(color);
+                    Log.e("RGB color code is: ", rgbString);
 
-    // Http call
+                    // Http call begin
+                    final String PARTICLE_DEVICE_ID = "310031000447353138383138";
+                    final String PARTICLE_TOKEN_ID = "1d89ba47f0c5c9ee72b7ebb12ac171bf5c1f9234";
+                    final String PARTICLE_API_URL = "https://api.particle.io/v1/devices/";
+                    AsyncHttpClient client = new AsyncHttpClient();
 
-                String PARTICLE_DEVICE_ID = "310031000447353138383138";
-                String PARTICLE_TOKEN_ID = "1d89ba47f0c5c9ee72b7ebb12ac171bf5c1f9234";
-                String PARTICLE_API_URL = "https://api.particle.io/v1/devices/";
-                AsyncHttpClient client = new AsyncHttpClient();
-                RequestParams params = new RequestParams();
-                params.put("access_token", PARTICLE_TOKEN_ID);
-                params.put("args", rgbString);
-                client.post(PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor", params, new TextHttpResponseHandler() {
-                            @Override
-                            public void onSuccess(int statusCode, Header[] headers, String res) {
-                                // called when response HTTP status is "200 OK"
-                                Log.e("Done","Cool! :D");
+                    final RequestParams params = new RequestParams();
+                    params.put("access_token", PARTICLE_TOKEN_ID);
+                    params.put("args", rgbString);
+                    client.post(PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor", params, new TextHttpResponseHandler() {
+
+                                @Override
+                                public void onSuccess(int statusCode, Header[] headers, String res) {
+                                    // Called when response HTTP status is "200 OK"
+                                    Log.i(LOG_TAG,"Lamp color was successfully changed. Cool! :D");
+                                    Toast.makeText(rootView.getContext(), colorSet, Toast.LENGTH_SHORT).show();
+                                    Snackbar.make(v, "Lamp color changed successfully, yey! :D", Snackbar.LENGTH_LONG).show();
+                                    spinner.setVisibility(View.INVISIBLE);
+                                }
+
+                                @Override
+                                public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                                    // Called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                                    Log.e(LOG_TAG, PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor" + params);
+                                    Toast.makeText(rootView.getContext(), PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor" + params, Toast.LENGTH_SHORT).show();
+                                    Snackbar.make(v, "A terrible error prevented lamp color change :(", Snackbar.LENGTH_LONG).show();
+                                    spinner.setVisibility(View.INVISIBLE);
+                                }
                             }
-
-                            @Override
-                            public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
-                                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                                Log.e("Dead","Not cool :(");
-                            }
-                        }
-                );
-
-                // Http call
-
-
-
-
-
-
-
-
-
-
-
-
+                    );
+                    // Http call end
+                }
 
             }
 
@@ -99,8 +105,4 @@ public class TabLight extends Fragment{
         return rootView;
 
     }
-
-
-
-
 }
