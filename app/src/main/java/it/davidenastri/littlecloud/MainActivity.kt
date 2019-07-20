e8e56504e560693c47b095c9799303cff7fe00c1
Davide Nastri <d.nastri@gmail.com>
2019-07-20 23:32:36 +0200

    feat: Added favourite color button / functionality in settings

--- app/src/main/java/it/davidenastri/littlecloud/MainActivity.kt
+++ app/src/main/java/it/davidenastri/littlecloud/MainActivity.kt
@@ -5,15 +5,21 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import top.defaults.colorpicker.ColorPickerView
+import top.defaults.colorpicker.ColorPickerPopup
+import android.app.Activity
+import android.app.PendingIntent.getActivity
+import android.util.Log
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,57 +47,73 @@ class MainActivity : AppCompatActivity() {
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_light -> {
-                colorPicker.visibility = View.VISIBLE;
-                musicPlayer.visibility = View.GONE;
-                settingsForm.visibility = View.GONE;
-
+                colorPicker.visibility = View.VISIBLE
+                musicPlayer.visibility = View.GONE
+                settingsForm.visibility = View.GONE
                 val tagLightStatus = fabButton.getTag(R.id.light_status)
                 val tagFabBgColor = fabButton.getTag(R.id.fab_bg_color) as Int
                 val tagFabIconColor = fabButton.getTag(R.id.fab_icon_color) as Int
                 fabButton.setBackgroundTintList(ColorStateList.valueOf(tagFabBgColor))
                 fabButton.setColorFilter(tagFabIconColor)
-
                 if (tagLightStatus.equals("lightOn")) {
-                    fabButton.hide();
-                    fabButton.setImageResource(R.drawable.ic_lightbulb_on_black);
-                    fabButton.show();
+                    fabButton.hide()
+                    fabButton.setImageResource(R.drawable.ic_lightbulb_on_black)
+                    fabButton.show()
                 } else {
-                    fabButton.hide();
-                    fabButton.setImageResource(R.drawable.ic_lightbulb_outline_black);
-                    fabButton.show();
+                    fabButton.hide()
+                    fabButton.setImageResource(R.drawable.ic_lightbulb_outline_black)
+                    fabButton.show()
                 }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_music -> {
-                colorPicker.visibility = View.GONE;
-                musicPlayer.visibility = View.VISIBLE;
-                settingsForm.visibility = View.GONE;
-                fabButton.hide();
+                colorPicker.visibility = View.GONE
+                musicPlayer.visibility = View.VISIBLE
+                settingsForm.visibility = View.GONE
+                fabButton.hide()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_settings -> {
-                val sharedPreference =  getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
-                var editTextParticleApiUrl = findViewById(R.id.particleApiUrl) as EditText
-                var editTextParticleDeviceId = findViewById(R.id.particleDeviceId) as EditText
-                var editTextParticleTokenId = findViewById(R.id.particleTokenId) as EditText
-                var editTextFavouriteColor = findViewById(R.id.favouriteColor) as EditText
+                val sharedPreference = getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
+                var editTextParticleApiUrl = findViewById(R.id.particle_api_url) as EditText
+                var editTextParticleDeviceId = findViewById(R.id.particle_device_id) as EditText
+                var editTextParticleTokenId = findViewById(R.id.particle_token_id) as EditText
+                var editTextFavouriteColor = findViewById(R.id.favourite_color) as EditText
+                val buttonFavouriteColor = findViewById(R.id.favourite_color_button) as Button
                 editTextParticleApiUrl.setText(sharedPreference.getString("particleApiUrl", "defaultName").toString())
-                editTextParticleDeviceId.setText(sharedPreference.getString("particleDeviceId", "defaultName").toString())
+                editTextParticleDeviceId.setText(
+                    sharedPreference.getString(
+                        "particleDeviceId",
+                        "defaultName"
+                    ).toString()
+                )
                 editTextParticleTokenId.setText(sharedPreference.getString("particleTokenId", "defaultName").toString())
                 editTextFavouriteColor.setText(sharedPreference.getString("favouriteColor", "defaultName").toString())
-                colorPicker.visibility = View.GONE;
-                musicPlayer.visibility = View.GONE;
-                settingsForm.visibility = View.VISIBLE;
-                fabButton.hide();
+                val favouriteColor = sharedPreference.getString("favouriteColor", "defaultName")
+                if (favouriteColor != null && favouriteColor != "") {
+                    val favouriteColor = Integer.parseInt(favouriteColor)
+                    editTextFavouriteColor.setTextColor(favouriteColor)
+                    buttonFavouriteColor.setBackgroundColor(favouriteColor)
+                    if (isColorDark(favouriteColor)) {
+                        buttonFavouriteColor.setTextColor(Color.WHITE)
+                    } else {
+                        buttonFavouriteColor.setTextColor(Color.BLACK)
+                    }
+                }
+                colorPicker.visibility = View.GONE
+                musicPlayer.visibility = View.GONE
+                settingsForm.visibility = View.VISIBLE
+                fabButton.hide()
                 fabButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_green_dark)))
                 fabButton.setColorFilter(Color.WHITE)
-                fabButton.setImageResource(R.drawable.ic_save_black);
-                fabButton.show();
+                fabButton.setImageResource(R.drawable.ic_save_black)
+                fabButton.show()
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -106,7 +128,7 @@ class MainActivity : AppCompatActivity() {
         fabButton.setTag(R.id.fab_icon_color, Color.BLACK)
         fabButton.setTag(R.id.fab_bg_color, Color.WHITE)
         colorPickerView.subscribe { color, _, shouldPropagate ->
-            if (isColorDark(color)){
+            if (isColorDark(color)) {
                 fabButton.setColorFilter(Color.WHITE)
                 fabButton.setTag(R.id.fab_icon_color, Color.WHITE)
             } else {
@@ -118,7 +140,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         val previousButton: ImageButton = findViewById(R.id.previous_song_button);
-        val previousButtonClickListener = View.OnClickListener {view ->
+        val previousButtonClickListener = View.OnClickListener { view ->
             when (view.getId()) {
                 R.id.previous_song_button ->
                     Snackbar.make(view, "Previous", Snackbar.LENGTH_SHORT).show()
@@ -128,7 +150,7 @@ class MainActivity : AppCompatActivity() {
         previousButton.setOnClickListener(previousButtonClickListener);
 
         val playButton: ImageButton = findViewById(R.id.play_pause_song_button);
-        val playButtonClickListener = View.OnClickListener {view ->
+        val playButtonClickListener = View.OnClickListener { view ->
             when (view.getId()) {
                 R.id.play_pause_song_button ->
                     Snackbar.make(view, "Play/Pause", Snackbar.LENGTH_SHORT).show()
@@ -138,14 +160,14 @@ class MainActivity : AppCompatActivity() {
         playButton.setOnClickListener(playButtonClickListener);
 
         val nextButton: ImageButton = findViewById(R.id.next_song_button);
-        val nextButtonClickListener = View.OnClickListener {view ->
+        val nextButtonClickListener = View.OnClickListener { view ->
             when (view.getId()) {
                 R.id.next_song_button ->
                     Snackbar.make(view, "Next", Snackbar.LENGTH_SHORT).show()
             }
         }
 
-        nextButton.setOnClickListener(nextButtonClickListener);
+        nextButton.setOnClickListener(nextButtonClickListener)
 
         val shortClickListenerFab = View.OnClickListener { view ->
             when (view.getId()) {
@@ -155,26 +177,26 @@ class MainActivity : AppCompatActivity() {
                         val tagLightStatus = fabButton.getTag(R.id.light_status) as String
                         if (tagLightStatus.equals("lightOn")) {
                             Snackbar.make(view, "Turning off...", Snackbar.LENGTH_SHORT).show()
-                            fabButton.hide();
-                            fabButton.setImageResource(R.drawable.ic_lightbulb_outline_black);
-                            fabButton.show();
+                            fabButton.hide()
+                            fabButton.setImageResource(R.drawable.ic_lightbulb_outline_black)
+                            fabButton.show()
                             fabButton.setTag(R.id.light_status, "lightOff")
                         } else {
                             Snackbar.make(view, "Turning on...", Snackbar.LENGTH_SHORT).show()
-                            fabButton.hide();
-                            fabButton.setImageResource(R.drawable.ic_lightbulb_on_black);
-                            fabButton.show();
+                            fabButton.hide()
+                            fabButton.setImageResource(R.drawable.ic_lightbulb_on_black)
+                            fabButton.show()
                             fabButton.setTag(R.id.light_status, "lightOn")
                         }
                     } else if (getSelectedItem(navView).equals(getString(R.string.title_settings))) {
                         // If pressed in Settings section
                         Snackbar.make(view, "I'll keep those settings in mind :)", Snackbar.LENGTH_SHORT).show()
-                        val sharedPreference =  getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
+                        val sharedPreference = getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
                         var editor = sharedPreference.edit()
-                        var editTextParticleApiUrl = findViewById(R.id.particleApiUrl) as EditText
-                        var editTextParticleDeviceId = findViewById(R.id.particleDeviceId) as EditText
-                        var editTextParticleTokenId = findViewById(R.id.particleTokenId) as EditText
-                        var editTextFavouriteColor = findViewById(R.id.favouriteColor) as EditText
+                        var editTextParticleApiUrl = findViewById(R.id.particle_api_url) as EditText
+                        var editTextParticleDeviceId = findViewById(R.id.particle_device_id) as EditText
+                        var editTextParticleTokenId = findViewById(R.id.particle_token_id) as EditText
+                        var editTextFavouriteColor = findViewById(R.id.favourite_color) as EditText
                         editor.putString("particleApiUrl", editTextParticleApiUrl.text.toString())
                         editor.putString("particleDeviceId", editTextParticleDeviceId.text.toString())
                         editor.putString("particleTokenId", editTextParticleTokenId.text.toString())
@@ -184,7 +206,43 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        fabButton.setOnClickListener(shortClickListenerFab);
+        fabButton.setOnClickListener(shortClickListenerFab)
+
+        val chooseFavouriteColorButton: Button = findViewById(R.id.favourite_color_button)
+        val chooseFavouriteColorButtonListener = View.OnClickListener { view ->
+            when (view.getId()) {
+                R.id.favourite_color_button ->
+                    ColorPickerPopup.Builder(this)
+                        .initialColor(Color.WHITE) // Set initial color
+                        .enableBrightness(true) // Enable brightness slider or not
+                        .enableAlpha(false) // Enable alpha slider or not
+                        .okTitle("Choose")
+                        .cancelTitle("Cancel")
+                        .showIndicator(false)
+                        .showValue(false)
+                        .build()
+                        .show(view, object : ColorPickerPopup.ColorPickerObserver() {
+                            override fun onColorPicked(color: Int) {
+                                chooseFavouriteColorButton.setBackgroundColor(color)
+                                if (isColorDark(color)) {
+                                    chooseFavouriteColorButton.setTextColor(Color.WHITE)
+                                } else {
+                                    chooseFavouriteColorButton.setTextColor(Color.BLACK)
+                                }
+
+                                val chooseFavouriteColorEditText: EditText = findViewById(R.id.favourite_color)
+                                chooseFavouriteColorEditText.setText(color.toString())
+                            }
+
+                            fun onColor(color: Int, fromUser: Boolean) {
+
+                            }
+                        })
+            }
+        }
+        chooseFavouriteColorButton.setOnClickListener(chooseFavouriteColorButtonListener)
 
     }
+
+
 }
