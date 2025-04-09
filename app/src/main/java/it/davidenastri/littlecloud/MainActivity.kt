af3eec262984c6ba11178a71860de0ad08512bf5
xt41vb <davide.nastri@ing.com>
2025-04-09 09:01:49 +0200

    feat: Improved QueryUtils

--- app/src/main/java/it/davidenastri/littlecloud/MainActivity.kt
+++ app/src/main/java/it/davidenastri/littlecloud/MainActivity.kt
@@ -4,14 +4,17 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.snackbar.Snackbar
-import it.davidenastri.littlecloud.databinding.ActivityMainBinding
-import androidx.core.view.size
 import androidx.core.view.get
+import androidx.core.view.isVisible
+import androidx.core.view.size
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import it.davidenastri.littlecloud.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,19 +22,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var currentVolume = 0
     private var isPlaying = false
+    private var lastFabButtonClickTime: Long = 0
+    private val debounceInterval: Long = 1000
 
     private fun getSelectedItem(bottomNavigationView: BottomNavigationView): String {
         val menu = bottomNavigationView.menu
-        for (i in 0 until bottomNavigationView.menu.size) {
+        for (i in 0 until menu.size) {
             val menuItem = menu[i]
             if (menuItem.isChecked) {
+                Log.d("SelectedItem", "Selected: ${menuItem.title}")
                 return menuItem.title.toString()
             }
         }
         return "none"
     }
 
-    fun isColorDark(color: Int): Boolean {
+    private fun isColorDark(color: Int): Boolean {
         val darkness = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
         return darkness > 0.5
     }
@@ -54,41 +60,100 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun handleSettingsNavigation() {
-        val sharedPreference = getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
-        binding.particleApiUrl.setText(sharedPreference.getString("particleApiUrl", "defaultName").toString())
-        binding.particleDeviceId.setText(sharedPreference.getString("particleDeviceId", "defaultName").toString())
-        binding.particleTokenId.setText(sharedPreference.getString("particleTokenId", "defaultName").toString())
-        binding.favouriteColor.setText(sharedPreference.getString("favouriteColor", "defaultName").toString())
-        val favouriteColor = sharedPreference.getString("favouriteColor", "defaultName")
-        if (favouriteColor != null && favouriteColor != "") {
-            val favouriteColorInt = Integer.parseInt(favouriteColor)
-            binding.favouriteColor.setTextColor(favouriteColorInt)
-            binding.favouriteColorButton.setBackgroundColor(favouriteColorInt)
-            if (isColorDark(favouriteColorInt)) {
-                binding.favouriteColorButton.setTextColor(Color.WHITE)
-            } else {
-                binding.favouriteColorButton.setTextColor(Color.BLACK)
+        val sharedPreferences = getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
+        with(binding) {
+            val defaultApiUrl = getString(R.string.particle_api_url)
+            val defaultDeviceId = getString(R.string.particle_device_id)
+            val defaultTokenId = getString(R.string.particle_token_id)
+            val defaultFavouriteColor = getString(R.string.favourite_color)
+
+            particleApiUrl.setText(sharedPreferences.getString("particle_api_url", ""))
+            particleDeviceId.setText(sharedPreferences.getString("particle_device_id", ""))
+            particleTokenId.setText(sharedPreferences.getString("particle_token_id", ""))
+            favouriteColor.setText(sharedPreferences.getString("favourite_color", ""))
+
+            setupField(particleApiUrl, defaultApiUrl)
+            setupField(particleDeviceId, defaultDeviceId)
+            setupField(particleTokenId, defaultTokenId)
+            setupField(favouriteColor, defaultFavouriteColor)
+
+            val favouriteColorValue = sharedPreferences.getString("favourite_color", "")
+            favouriteColorValue?.let {
+                if (it.all { char -> char.isDigit() }) {
+                    val favouriteColorInt = it.toInt()
+                    favouriteColor.setTextColor(favouriteColorInt)
+                    favouriteColorButton.setBackgroundColor(favouriteColorInt)
+                    favouriteColorButton.setTextColor(if (isColorDark(favouriteColorInt)) Color.WHITE else Color.BLACK)
+                }
+            }
+
+            colorPicker.visibility = View.GONE
+            musicPlayer.visibility = View.GONE
+            settingsForm.visibility = View.VISIBLE
+
+            fabButton.apply {
+                hide()
+                backgroundTintList = ColorStateList.valueOf(resources.getColor(android.R.color.holo_green_dark))
+                setColorFilter(Color.WHITE)
+                setImageResource(R.drawable.ic_save_black)
+                setOnClickListener { handleFabButtonClick(it) }
+                show()
             }
         }
-        binding.colorPicker.visibility = View.GONE
-        binding.musicPlayer.visibility = View.GONE
-        binding.settingsForm.visibility = View.VISIBLE
-        binding.fabButton.hide()
-        binding.fabButton.setBackgroundTintList(ColorStateList.valueOf(resources.getColor(android.R.color.holo_green_dark)))
-        binding.fabButton.setColorFilter(Color.WHITE)
-        binding.fabButton.setImageResource(R.drawable.ic_save_black)
-        binding.fabButton.show()
     }
 
+    /**
+     * Sets up EditText behavior for the settings screen.
+     * - When focused: if the text is default or empty, clears text and sets color to black.
+     * - When focus is lost: if the text is empty, resets to default text and sets color to gray.
+     */
+    private fun setupField(editText: EditText, defaultText: String) {
+        editText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                if (editText.text.toString() == defaultText || editText.text.isEmpty()) {
+                    editText.setText("")
+                    editText.setTextColor(Color.BLACK)
+                }
+            } else {
+                if (editText.text.isEmpty()) {
+                    editText.setText(defaultText)
+                    editText.setTextColor(Color.GRAY)
+                }
+            }
+        }
+    }
+
+    private fun updateTextColor(editText: EditText, defaultText: String) {
+        editText.setTextColor(if (editText.text.toString() == defaultText || editText.text.isEmpty()) Color.GRAY else Color.BLACK)
+    }
+
+    /**
+     * Handles navigation for music settings.
+     * - Hides color picker and settings form.
+     * - Shows music player.
+     * - Hides FAB button.
+     */
     private fun handleMusicNavigation() {
+        // Hide color picker and settings form
         binding.colorPicker.visibility = View.GONE
-        binding.musicPlayer.visibility = View.VISIBLE
         binding.settingsForm.visibility = View.GONE
+
+        // Show music player
+        binding.musicPlayer.visibility = View.VISIBLE
+
+        // Hide FAB button
         binding.fabButton.hide()
     }
 
+
+    /**
+     * Handles navigation for light settings.
+     * - Shows color picker and hides music player and settings form.
+     * - Sets FAB button background and icon colors based on tags.
+     * - If light is on, updates FAB button to show "light on" icon.
+     * - If light is off, updates FAB button to show "light off" icon.
+     */
     private fun handleLightNavigation() {
         binding.colorPicker.visibility = View.VISIBLE
         binding.musicPlayer.visibility = View.GONE
@@ -135,7 +200,7 @@ class MainActivity : AppCompatActivity() {
                 binding.fabButton.setColorFilter(Color.BLACK)
                 binding.fabButton.setTag(R.id.fab_icon_color, Color.BLACK)
             }
-            binding.fabButton.setBackgroundTintList(ColorStateList.valueOf(color))
+            binding.fabButton.backgroundTintList = ColorStateList.valueOf(color)
             binding.fabButton.setTag(R.id.fab_bg_color, color)
         }
     }
@@ -152,7 +217,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.fabButton.setOnLongClickListener {
-            if (binding.colorPicker.visibility == View.VISIBLE) {
+            if (binding.colorPicker.isVisible) {
                 Toast.makeText(this, "Long click detected", Toast.LENGTH_SHORT).show()
             }
             true
@@ -160,48 +225,67 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleFabButtonClick(view: View) {
-        if (getSelectedItem(binding.navView) == getString(R.string.title_light)) {
-            val tagLightStatus = binding.fabButton.getTag(R.id.light_status) as String
-            if (tagLightStatus == "lightOn") {
-                val rgbString = "0,0,0,1000"
-                val colorSet = "Red: 0 Green: 0 Blue: 0"
-                QueryUtils.changeColor(rgbString, colorSet, binding.fabButton)
-                Snackbar.make(view, "Turning off...", Snackbar.LENGTH_SHORT).show()
-                binding.fabButton.hide()
-                binding.fabButton.setImageResource(R.drawable.ic_lightbulb_outline_black)
-                binding.fabButton.show()
-                binding.fabButton.setTag(R.id.light_status, "lightOff")
-            } else {
-                val chosenColor: Int = binding.fabButton.getTag(R.id.fab_bg_color) as Int
-                val red = Color.red(chosenColor)
-                val green = Color.green(chosenColor)
-                val blue = Color.blue(chosenColor)
-                val rgbString = "$red,$green,$blue,1000"
-                val colorSet = "Red: $red Green: $green Blue: $blue"
-                QueryUtils.changeColor(rgbString, colorSet, binding.fabButton)
-                Snackbar.make(view, "Turning on...", Snackbar.LENGTH_SHORT).show()
-                binding.fabButton.hide()
-                binding.fabButton.setImageResource(R.drawable.ic_lightbulb_on_black)
-                binding.fabButton.show()
-                binding.fabButton.setTag(R.id.light_status, "lightOn")
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - lastFabButtonClickTime < this.debounceInterval) {
+            Toast.makeText(view.context, "Please wait...", Toast.LENGTH_SHORT).show()
+            return
+        }
+        lastFabButtonClickTime = currentTime
+
+        val selectedItem = getSelectedItem(binding.navView)
+        Log.d("handleFabButtonClick", "Selected item: $selectedItem")
+
+        when (selectedItem) {
+            getString(R.string.title_light) -> {
+                Log.d("handleFabButtonClick", "Light screen active")
+                val tagLightStatus = binding.fabButton.getTag(R.id.light_status) as String
+                if (tagLightStatus == "lightOn") {
+                    val rgbString = "0,0,0,1000"
+                    val colorSet = "Red: 0 Green: 0 Blue: 0"
+                    Toast.makeText(view.context, "Turning off...", Toast.LENGTH_SHORT).show()
+                    binding.fabButton.hide()
+                    binding.fabButton.setImageResource(R.drawable.ic_lightbulb_outline_black)
+                    binding.fabButton.show()
+                    binding.fabButton.setTag(R.id.light_status, "lightOff")
+                } else {
+                    val chosenColor: Int = binding.fabButton.getTag(R.id.fab_bg_color) as Int
+                    val red = Color.red(chosenColor)
+                    val green = Color.green(chosenColor)
+                    val blue = Color.blue(chosenColor)
+                    val rgbString = "$red,$green,$blue,1000"
+                    val colorSet = "Red: $red Green: $green Blue: $blue"
+                    Toast.makeText(view.context, "Turning on...", Toast.LENGTH_SHORT).show()
+                    binding.fabButton.hide()
+                    binding.fabButton.setImageResource(R.drawable.ic_lightbulb_on_black)
+                    binding.fabButton.show()
+                    binding.fabButton.setTag(R.id.light_status, "lightOn")
+                }
             }
-        } else if (getSelectedItem(binding.navView) == getString(R.string.title_settings)) {
-            Snackbar.make(view, "I'll keep those settings in mind :)", Snackbar.LENGTH_SHORT).show()
-            val sharedPreference = getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
-            val editor = sharedPreference.edit()
-            editor.putString("particleApiUrl", binding.particleApiUrl.text.toString())
-            editor.putString("particleDeviceId", binding.particleDeviceId.text.toString())
-            editor.putString("particleTokenId", binding.particleTokenId.text.toString())
-            editor.putString("favouriteColor", binding.favouriteColor.text.toString())
-            editor.apply()
+            getString(R.string.title_settings) -> {
+                Log.d("handleFabButtonClick", "Settings screen active")
+                Toast.makeText(view.context, "I'll keep those settings in mind :)", Toast.LENGTH_SHORT).show()
+                val sharedPreference = getSharedPreferences("SETTINGS", Context.MODE_PRIVATE)
+                val editor = sharedPreference.edit()
+                editor.putString("particleApiUrl", binding.particleApiUrl.text.toString())
+                editor.putString("particleDeviceId", binding.particleDeviceId.text.toString())
+                editor.putString("particleTokenId", binding.particleTokenId.text.toString())
+                editor.putString("favouriteColor", binding.favouriteColor.text.toString())
+                editor.apply()
+            }
+            else -> {
+                Log.d("handleFabButtonClick", "Unknown screen active")
+                Toast.makeText(view.context, "Unknown screen", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
+
 
     private fun setupMusicControls() {
         binding.previousSongButton.setOnClickListener { view ->
             when (view.id) {
                 R.id.previous_song_button -> {
-                    Snackbar.make(view, "Previous", Snackbar.LENGTH_SHORT).show()
+                    Toast.makeText(view.context, "Previous", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -212,11 +296,11 @@ class MainActivity : AppCompatActivity() {
                     if (isPlaying) {
                         isPlaying = false
                         binding.playPauseSongButton.setImageResource(R.drawable.ic_pause_black)
-                        QueryUtils.changeAudio("pause,$currentVolume", view)
+                        //QueryUtils.changeAudio("pause,$currentVolume", view)
                     } else {
                         isPlaying = true
                         binding.playPauseSongButton.setImageResource(R.drawable.ic_play_arrow_black)
-                        QueryUtils.changeAudio("play,$currentVolume", view)
+                        //QueryUtils.changeAudio("play,$currentVolume", view)
                     }
                 }
             }
@@ -225,7 +309,7 @@ class MainActivity : AppCompatActivity() {
         binding.nextSongButton.setOnClickListener { view ->
             when (view.id) {
                 R.id.next_song_button -> {
-                    QueryUtils.changeAudio("playNext,$currentVolume", view)
+                    //QueryUtils.changeAudio("playNext,$currentVolume", view)
                 }
             }
         }
@@ -235,7 +319,7 @@ class MainActivity : AppCompatActivity() {
         binding.volumeSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 currentVolume = i
-                QueryUtils.changeAudio("setVolume,$i", binding.volumeSeekbar)
+                // QueryUtils.changeAudio("setVolume,$i", binding.volumeSeekbar)
                 Toast.makeText(applicationContext, "Volume: $i", Toast.LENGTH_SHORT).show()
             }
 
--- app/src/main/java/it/davidenastri/littlecloud/QueryUtils.java
+++ app/src/main/java/it/davidenastri/littlecloud/QueryUtils.java
@@ -4,85 +4,87 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
+
 import com.google.android.material.snackbar.Snackbar;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
-import cz.msebera.android.httpclient.Header;
 
-import static com.loopj.android.http.AsyncHttpClient.LOG_TAG;
+import cz.msebera.android.httpclient.Header;
 
 public final class QueryUtils {
 
-    /**
-     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
-     * This class is only meant to hold static variables and methods, which can be accessed
-     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
-     */
+    private static final String SETTINGS = "SETTINGS";
+    private static final String PARTICLE_DEVICE_ID_KEY = "particle_device_id";
+    private static final String PARTICLE_TOKEN_ID_KEY = "particle_token_id";
+    private static final String PARTICLE_API_URL_KEY = "particle_token_url";
+    private static final String DEFAULT_NAME = "defaultName";
+    private static final String LOG_TAG = "QueryUtils";
+
     private QueryUtils() {
     }
 
+    private static SharedPreferences getSharedPreferences(View view) {
+        return view.getContext().getSharedPreferences(SETTINGS, Context.MODE_PRIVATE);
+    }
+
+    private static String[] getParticleDetails(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences(view);
+        String deviceId = sharedPreferences.getString(PARTICLE_DEVICE_ID_KEY, DEFAULT_NAME);
+        String tokenId = sharedPreferences.getString(PARTICLE_TOKEN_ID_KEY, DEFAULT_NAME);
+        String apiUrl = sharedPreferences.getString(PARTICLE_API_URL_KEY, DEFAULT_NAME);
+        return new String[]{deviceId, tokenId, apiUrl};
+    }
+
+    private static RequestParams createRequestParams(String tokenId, String args) {
+        RequestParams params = new RequestParams();
+        params.put("access_token", tokenId);
+        params.put("args", args);
+        return params;
+    }
 
     public static void changeColor(String rgbString, final String colorSet, final View onClickView) {
+        String[] particleDetails = getParticleDetails(onClickView);
+        String deviceId = particleDetails[0];
+        String tokenId = particleDetails[1];
+        String apiUrl = particleDetails[2];
+        RequestParams params = createRequestParams(tokenId, rgbString);
         AsyncHttpClient client = new AsyncHttpClient();
-        SharedPreferences sharedPreference = onClickView.getContext().getSharedPreferences("SETTINGS", Context.MODE_PRIVATE);
-        final String PARTICLE_DEVICE_ID = sharedPreference.getString("particleDeviceId", "defaultName");
-        final String PARTICLE_TOKEN_ID = sharedPreference.getString("particleTokenId", "defaultName");
-        final String PARTICLE_API_URL = sharedPreference.getString("particleApiUrl", "defaultName");
-        final RequestParams params = new RequestParams();
-        params.put("access_token", PARTICLE_TOKEN_ID);
-        params.put("args", rgbString);
-        client.post(PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor", params, new TextHttpResponseHandler() {
+
+        client.post(apiUrl + deviceId + "/setColor", params, new TextHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, String res) {
-                // called when response HTTP status is "200 OK"
-                // Print the chosen color to log
-                Log.i(LOG_TAG, colorSet);
-                //Toast.makeText(tabLightView.getContext(), colorSet, Toast.LENGTH_SHORT).show();
-                //Snackbar.make(onClickView, "Lamp color changed successfully, yey! :D", Snackbar.LENGTH_LONG).show();
-                //spinner.setVisibility(View.INVISIBLE);
+                Log.i(LOG_TAG, "Color set to: " + colorSet);
+                Snackbar.make(onClickView, "Lamp color changed successfully!", Snackbar.LENGTH_LONG).show();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                Log.e(LOG_TAG, PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor" + params);
-                //Toast.makeText(tabLightView.getContext(), PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/setColor" + params, Toast.LENGTH_SHORT).show();
-                Snackbar.make(onClickView, "Little Cloud is offline... :(", Snackbar.LENGTH_LONG).show();
-                //spinner.setVisibility(View.INVISIBLE);
+                Log.e(LOG_TAG, "Failed to set color: " + res);
+                Snackbar.make(onClickView, "Little Cloud is offline...", Snackbar.LENGTH_LONG).show();
             }
         });
     }
 
-
     public static void changeAudio(final String commandString, final View onClickView) {
+        String[] particleDetails = getParticleDetails(onClickView);
+        String deviceId = particleDetails[0];
+        String tokenId = particleDetails[1];
+        String apiUrl = particleDetails[2];
+        RequestParams params = createRequestParams(tokenId, commandString);
         AsyncHttpClient client = new AsyncHttpClient();
-        SharedPreferences sharedPreference = onClickView.getContext().getSharedPreferences("SETTINGS", Context.MODE_PRIVATE);
-        final String PARTICLE_DEVICE_ID = sharedPreference.getString("particleDeviceId", "defaultName");
-        final String PARTICLE_TOKEN_ID = sharedPreference.getString("particleTokenId", "defaultName");
-        final String PARTICLE_API_URL = sharedPreference.getString("particleApiUrl", "defaultName");
-        final RequestParams params = new RequestParams();
-        params.put("access_token", PARTICLE_TOKEN_ID);
-        params.put("args", commandString);
-        Log.i("changeAudio:", commandString);
-        client.post(PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/dfMini", params, new TextHttpResponseHandler() {
+
+        client.post(apiUrl + deviceId + "/dfMini", params, new TextHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, String res) {
-                Log.i(LOG_TAG, PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/dfMini  " + params);
-                Log.i(LOG_TAG, "Response code: " + statusCode);
-                Log.i(LOG_TAG, "Response: " + res);
-                //Toast.makeText(tabSoundView.getContext(), colorSet, Toast.LENGTH_SHORT).show();
-                //Snackbar.make(onClickView, "Lamp audio changed successfully, yey! :D", Snackbar.LENGTH_LONG).show();
+                Log.i(LOG_TAG, "Audio command sent: " + commandString);
+                Snackbar.make(onClickView, "Lamp audio changed successfully!", Snackbar.LENGTH_LONG).show();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                Log.e(LOG_TAG, PARTICLE_API_URL + PARTICLE_DEVICE_ID + "/dfMini  " + params);
-                Log.e(LOG_TAG, "Response code: " + statusCode);
-                Log.e(LOG_TAG, "Response: " + res);
-                Snackbar.make(onClickView, "Little Cloud is offline... :(", Snackbar.LENGTH_LONG).show();
+                Log.e(LOG_TAG, "Failed to change audio: " + res);
+                Snackbar.make(onClickView, "Little Cloud is offline...", Snackbar.LENGTH_LONG).show();
             }
         });
     }
